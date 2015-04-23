3ca699c15988367be541c160234d42794e7b0b03
Wendy Kan <wendykan@gmail.com>
2015-04-22 18:53:25 -0700

    plot2

--- proj2/main.R
+++ proj2/main.R
@@ -7,3 +7,6 @@ SCC <- readRDS("exdata-data-NEI_data/Source_Classification_Code.rds")
 
 str(NEI)
 plot(summarise(group_by(NEI, year), Emissions=sum(Emissions)))
+plot(summarise(group_by(NEI[which(NEI$fips == "24510"),], year), Emissions=sum(Emissions)))
+
+unique(NEI$type)
